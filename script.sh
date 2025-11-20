8592121470700a4eb7b1182016ff0cd3baf1c42d
Mannaz76 <artem.bezzubtsev@yandex.ru>
2025-11-20 11:55:22 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+apt update && apt install zfsutils-linux
+
 zpool create test1 mirror /dev/sdb /dev/sdc
 zpool create test2 mirror /dev/sdd /dev/sde
 zpool create test3 mirror /dev/sdf /dev/sdg
@@ -15,3 +19,10 @@ zfs set compression=lz4 test4/1
 
 for i in {1..4}; do cp -r /var/log/* /test$i/1 ; done
 
+wget -O archive.tar.gz --no-check-certificate 'https://drive.usercontent.google.com/download?id=1MvrcEp-WgAQe57aDEzxSRalPAwbNN1Bb&export=download'
+tar -xzvf archive.tar.gz
+zpool import -d zpoolexport/ otus
+zpool status
+
+wget -O otus_task2.file --no-check-certificate https://drive.usercontent.google.com/download?id=1wgxjih8YZ-cqLqaZVa0lA3h3Y029c3oI&export=download
+zfs receive otus/test@now < otus_task2.file
